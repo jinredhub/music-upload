b8824d5065a6474a11281ea841416db9bf7c1718
jinredhub <jin@nieblo.com>
2019-10-26 10:48:03 -0700

    build

---
+++ client/build/precache-manifest.20b033cd80d3f6a13fabc2f7f4be48cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f007f7ca048e8f1ea92e620b02409ce9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f12c5002b68e140438",
+    "url": "/static/css/main.411fc9e0.chunk.css"
+  },
+  {
+    "revision": "7af982f6cb684edd06c3",
+    "url": "/static/js/2.45c39f29.chunk.js"
+  },
+  {
+    "revision": "c3f12c5002b68e140438",
+    "url": "/static/js/main.67ff2204.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
